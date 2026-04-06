96d3d722f69ca3d3ac590825f9c37986cc80e974
gitedelormoy <Pascale.pons6@wanadoo.fr>
2026-04-06 19:43:51 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://gitedelormoy.github.io',
-  base: '/gite',
   integrations: [
     react(),
     tailwind({ applyBaseStyles: false }),
